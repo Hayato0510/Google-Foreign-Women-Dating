4a7afba35005ad75d1129fa20ca64b74327ad81a
EC2 Default User <ec2-user@ip-172-31-2-104.us-east-2.compute.internal>
2020-07-22 10:32:27 +0000

    mm

---
+++ resources/views/google/noMatch.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>テスト</title>
+        <link href="{{ secure_asset('css/noMatch.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 
 // LP獲得用
-Route::get('jobs', function () {
+Route::get('live', function () {
     return view('google.check'); //仮LP
     // return redirect('https://a-trade.jp/redirect/charin?media=G16531'); //チャリン - Aトレード
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('google.check'); //仮LP
+    return view('google.noMatch'); //獲得LP(1)
 });
