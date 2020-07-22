4c960cdd4abba0df221dbd91dba82bb6eca37350
EC2 Default User <ec2-user@ip-172-31-2-104.us-east-2.compute.internal>
2020-07-22 23:21:17 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 // LP獲得用
 Route::get('matching', function () {
-    return view('google.check'); //仮LP
-    // return view('google.noMatch'); //獲得LP(1)
+    // return view('google.check'); //仮LP
+    return view('google.noMatch'); //獲得LP(1)
     
     // return redirect('https://a-trade.jp/redirect/charin?media=G16531'); //チャリン - Aトレード
 });
